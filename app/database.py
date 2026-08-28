1a82ebf9eabea491b83c68b4a2a021f2c5d8aa11
Son To <toson606@gmail.com>
2026-08-28 13:44:11 +0700

    Fix database configuration

--- app/database.py
+++ app/database.py
@@ -22,35 +22,11 @@ def get_db():
         db.close()
 
 
-if __name__ == "__main__":
-    with engine.connect() as connection:
-        print("Kết nối PostgreSQL thành công!")
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:1@localhost:5432/employee_db"
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-# Tạo các bảng trong database
 def create_tables():
     from . import models
     Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    with engine.connect() as connection:
+        print("Kết nối PostgreSQL thành công!")
